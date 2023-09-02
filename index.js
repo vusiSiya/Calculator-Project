2612c28aef4bb23b06e5a00ca935c4caf637517b
Siyabonga Mahlalela <siyabonga.mahlalela@imaginescholar.org>
2023-09-02 23:08:23 +0200

    grid miracle and rendering buttons faster!

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const inputEl = document.querySelector(".input-El");
 const answerEl = document.querySelector(".answer-El");
 const numberBtns = document.querySelector(".number-btns");
+
 const operatorBtns = document.querySelector(".operator-btns");
+const btnsSection = document.querySelector(".all--buttons");
 let operatorIsClicked = false;
 let equalBtnPressed = false;
 let showAnswer = false;
@@ -9,84 +11,92 @@ let currentNum = "";
 let firstNum;
 let secondNum;
 let operator;
-let className; 
+let className;
 
-renderBtnElements(numberBtns,operatorBtns);
+renderBtnElements(numberBtns, operatorBtns);
 
 document.addEventListener("click", (e)=>{
-	const {className, textContent} = e.target;
-	let value = textContent;
-	if (className === "num") {
-		currentNum = (firstNum && operatorIsClicked) ? value : currentNum + value;
-	} else if (className === "sign") {
-		operatorIsClicked = true;
-		operator = value;
-		/*equalBtnPressed = operator === "=" ? !equalBtnPressed : equalBtnPressed;
+    const {className, textContent} = e.target;
+    let value = textContent;
+    if (className === "num") {
+        currentNum = (firstNum && operatorIsClicked) ? value : currentNum + value;
+    } else if (className === "sign") {
+        operatorIsClicked = true;
+        operator = value;
+        /*equalBtnPressed = operator === "=" ? !equalBtnPressed : equalBtnPressed;
 		return inputEl.value = getAnswer();*/
-	}
-	operatorIsClicked && (currentNum == "" || value != operator) ?
-		secondNum = currentNum * 1
-		:
-		firstNum = currentNum * 1;
-	inputEl.value = (secondNum) ? getAnswer(operator) : currentNum;
-});
+    }
+    operatorIsClicked && (currentNum == "" || value != operator) ? secondNum = currentNum * 1 : firstNum = currentNum * 1;
+    inputEl.value = (secondNum) ? getAnswer(operator) : currentNum;
+}
+);
 
-function renderBtnElements(_numberBtns,_operatorBtns) {
+function renderBtnElements(_numberBtns, _operatorBtns) {
+	// #new approach!! #new way!! #faster!
+    const characters = createArray("789CD654/*321+-0.%=");
+    btnsSection.innerHTML = characters.reduce((acc,char)=>{
+        return acc + `<button type="text" >${char}</button>`
+    }, "");
+	btnsSection.children[characters.indexOf("=")].style = "grid-column: -3/-1";
+
+    /*
 	const numbers = createArray("789654321");
 	const operators = createArray("/*+-=");
 	const numbersFragment = appendChildren(elementsArray(numbers, "num"));
 	const operatorsFragment = appendChildren(elementsArray(operators, "sign"));
 	_numberBtns.appendChild(numbersFragment);
 	_operatorBtns.appendChild(operatorsFragment);
+	*/
 }
 function createArray(_string) {
-	let newArray = [];
-	for (let char of _string) {
-		newArray.push(char);
-	}
-	return newArray;
+    let newArray = [];
+    for (let char of _string) {
+        newArray.push(char);
+    }
+    return newArray;
 }
 function elementsArray(_array, _className) {
-	className = _className;
-	const elements = _array.map((arrayEl,i)=>{
-		let newElement = createEl("button", _className, arrayEl);
-		return newElement;
-	})
-	return elements;
+    className = _className;
+    const elements = _array.map((arrayEl,i)=>{
+        let newElement = createEl("button", _className, arrayEl);
+        return newElement;
+    }
+    )
+    return elements;
 }
 
 function createEl(_el, _className, _text) {
-	let newElement = document.createElement(_el);
-	newElement.innerText = _text ? _text : "";
-	newElement.className = _className;
-	return newElement
+    let newElement = document.createElement(_el);
+    newElement.innerText = _text ? _text : "";
+    newElement.className = _className;
+    return newElement
 }
 
 function appendChildren(_elementsArray) {
-	let fragment = document.createDocumentFragment();
-	_elementsArray.forEach(el => fragment.appendChild(el));
-	return fragment;
+    let fragment = document.createDocumentFragment();
+    _elementsArray.forEach(el=>fragment.appendChild(el));
+    return fragment;
 }
 
 function getAnswer(_operator) {
-	let answer = 0;
-	switch (_operator) {
-	case "/":
-		answer = firstNum / secondNum;
-		break;
-	case "*":
-		answer = firstNum * secondNum;
-		break;
-	case "-":
-		answer = firstNum - secondNum;
-		break;
-	case "+":
-		answer = firstNum + secondNum;
-		break;
-	default:
-		break;
-	}
-	return answer * 1;
+    let answer = 0;
+    switch (_operator) {
+    case "/":
+        answer = firstNum / secondNum;
+        break;
+    case "*":
+        answer = firstNum * secondNum;
+        break;
+    case "-":
+        answer = firstNum - secondNum;
+        break;
+    case "+":
+        answer = firstNum + secondNum;
+        break;
+    default:
+        break;
+    }
+    return answer * 1;
 }
 
 // Handling button numbers state:
