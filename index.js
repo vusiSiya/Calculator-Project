b32858efae36f644c8a3fe710516a219689739c2
Siyabonga Mahlalela <siyabonga.mahlalela@imaginescholar.org>
2023-09-10 21:41:37 +0200

    removing comments _ fixing errors_

--- index.js
+++ index.js
@@ -2,30 +2,25 @@ const inputEl = document.querySelector(".input-El");
 const answerEl = document.querySelector(".answer-El");
 const btnsSection = document.querySelector(".all--buttons");
 let operatorIsClicked = false;
-//let equalBtnClicked = false;
 let showAnswer = false;
 let currentNum = "";
 let firstNum;
 let secondNum;
-let operator;
+let operator; // let the operator be local, not global!
 let className;
-let answer
+let answer = 0;
 
 renderBtnElements();
 
-function sideEffectsBtn(btnText) {
-    if ((btnText === "=") || (btnText === "Del") || (btnText === "AC")) {
-        return true;
-    }
-    return false;
-}
 function handleClick(e) {
+
     const {textContent} = e.target;
-    if (!sideEffectsBtn(textContent)) {
-        let value = parseInt(textContent)
+    
+    if (!sideEffectsBtn(textContent) ) {
+        let value = parseInt(textContent) 
         let newOperator;
 
-        if (value) {
+        if (value || textContent === ".") {
             currentNum = (firstNum && operator) ? value : currentNum + value
 
         } else {
@@ -37,138 +32,119 @@ function handleClick(e) {
             firstNum = answer;
             secondNum = value;
         } else {
-            operator ? secondNum = value : firstNum = parseInt(currentNum);
+            operator ? secondNum = value : firstNum = parseFloat(currentNum);
         }
-        inputEl.value += !sideEffectsBtn(textContent) && textContent || "";
-
-        answer = secondNum && getAnswer(operator, firstNum, secondNum);
         displayAnswer(false);
 
     } else {
         switch (textContent) {
-        case "=":
-            displayAnswer(true);
+            case "=":
+                displayAnswer(true)
+                break;
+            case "Del":
+                deleteLast();
+                break;
+            case "AC":
+                deleteAll();
+                break;
+            case ".":
+                convertToDecimal(textContent);
             break;
-        case "Del":
-            deleteAll();
-            break;
-        case "AC":
-            deleteAll();
-            break;
-        default:
-            break;
-        }
+            default:
+                break;
+            }
     }
+
+        inputEl.value += !sideEffectsBtn(textContent) && textContent || "";
+        answer = secondNum && getAnswer(operator, firstNum, secondNum);
 }
 
-function displayAnswer(isClicked) {
-    console.log("answer btn clicked")
-    answerEl.textContent = answer || "";
-    answerEl.style = isClicked ? "color: black; font-weight: bold;" : "color: grey;font-weight: normal;"
-}
+function renderBtnElements() {
+    const buttons = createArray("789CD654÷×321+-0.%=");
 
-    // #new approach!! #new way!! #faster!
-    const characters = createArray("789DC654÷x321+-0.%=");
-    btnsSection.innerHTML = characters.reduce((acc,char)=>{
-        return acc + `<button type="text" >${char}</button>`
+    btnsSection.innerHTML = buttons.reduce((acc,char)=>{
         return acc + `<button type="button" >${char}</button>`
-    }, "");
+    }
+    , "");
 
-	function targetBtn(char) {
+    function targetBtn(char) {
+        return btnsSection.children[buttons.indexOf(char)]
     }
 
     targetBtn("=").style = "grid-column: -3/-1";
     targetBtn("C").textContent = "AC";
     targetBtn("D").textContent = "Del";
-
-    let btnsArray = [...btnsSection.children]
+    let btnsArray = [...btnsSection.children];
     btnsArray.forEach((btn)=>{
         return btn.addEventListener("click", (e)=>handleClick(e))
-    });
+    }
+    );
 }
 
-   })
-    /*
-	const numbers = createArray("789654321");
-	const operators = createArray("/*+-=");
-	const numbersFragment = appendChildren(elementsArray(numbers, "num"));
-	const operatorsFragment = appendChildren(elementsArray(operators, "sign"));
-	_numberBtns.appendChild(numbersFragment);
-	_operatorBtns.appendChild(operatorsFragment);
-	*/
 function createArray(_string) {
-    case "%":
+    let newArray = [];
     for (let char of _string) {
+        newArray.push(char)
+    }
+    return newArray;
+}
+
+function sideEffectsBtn(btnText) {
+    if ((btnText === "=") || (btnText === "Del") || (btnText === "AC")) {
+        return true;
+    }
+    return false;
+}
+
+function convertToDecimal(textContent) {
+    parseFloat(currentNum += textContent)
+}
+
+function displayAnswer(isClicked) {
+    const conditionalStyle = ( isClicked ?
+        "color: black; font-weight: bold;" 
+        : 
+        "color: grey;font-weight: normal; font-size: 1.3rem"
+    );
+}
+    let isZero = (answer === 0)
+    answerEl.textContent = isZero ? 0 : answer || "";
+
+    answerEl.style = conditionalStyle;
+function deleteLast() {
+    
+}
+function deleteAll() {
+    firstNum *=0;
+    secondNum *=0;
+    currentNum =""
+    operator =""
+    answer = 0;
+    inputEl.value =""
+    answerEl.textContent = 0;
+}
+function getAnswer(_operator, firstNum=0, secondNum=0) {
+    let answer
+    switch (_operator) {
+    case "%":
+        answer = firstNum % secondNum
         break;
     case "÷":
-        return firstNum / secondNum
+        answer = firstNum / secondNum
         break;
-    case "x":
-        return firstNum * secondNum
+    case "×":
+        answer = firstNum * secondNum
         break;
     case "-":
-        return firstNum - secondNum
+        answer = firstNum - secondNum
         break;
     case "+":
-        return firstNum + secondNum
+        answer = firstNum + secondNum
         break;
     default:
         break;
     }
-}
-function createEl(_el, _className, _text) {
-    let newElement = document.createElement(_el);
-    newElement.innerText = _text ? _text : "";
-    newElement.className = _className;
-    return newElement
+
+    return parseFloat(answer)
 }
 
-function appendChildren(_elementsArray) {
-    let fragment = document.createDocumentFragment();
-    _elementsArray.forEach(el=>fragment.appendChild(el));
-    return fragment;
-}
-
-function getAnswer(_operator, firstNum, secondNum=0) {
-        return firstNum % secondNum
-	case "%":
-     return firstNum % secondNum
-        break;
-    case "/":
-     return firstNum / secondNum
-        break;
-    case "*":
-     return firstNum * secondNum
-        break;
-    case "-":
-     return firstNum - secondNum
-        break;
-    case "+":
-     return firstNum + secondNum
-        break;
-    default:
-        break;
-
-// Handling button numbers state:
-
-// Recieving Input and returning an answer.
-// recieving an integer, as a string,
-// we keep adding to number characters to  string until the user clicks an operator button.
-// when user clicks the operator button, we multiply the currentNum string by 1, turning it to an integer; 
-// we save the operator character, and save the currentNum char as the firstNum;
-// the currentNum string is then initialised to an empty string.
-// the next numbers the user clicks will be saved to the emptied currentNum string;
-/*
-            When the user clicks another operator,
-                we save initialise secondNum to the currentNum string;
-                we return answer for the firstNum and secondNum values;
-                after we return the answer, we empty the currentNum string, and the secondNum value;
-                we initialise the firstNum to the answer.
-                the process of recieving input as a string until the operator is pressed is started over.
-             */
-/*
-            The user may also click on the equal sign after entering the firstNum and secondNum values.
-                when this happens, we return answer for the firstNum and secondNum;
-                we save the answer, firstNum and secondNum values to an object, and push it to an array, that will save all calculations data.
-                after this, we initialise firstNum, and secondNum values to zero, so as the answer, if it is a global variable.
-        */
